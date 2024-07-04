030b536754c58fac7dd3e1d12118a0788c7f92f1
Amirhosseinfsh <amirhosseinfsh2000@gmail.com>
2024-07-04 16:19:41 +0330

    initial commit

---
+++ src/test/java/org/webproject/mainsystem/MainSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package org.webproject.mainsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
